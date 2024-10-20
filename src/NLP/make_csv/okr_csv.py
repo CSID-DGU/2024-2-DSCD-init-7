9f48b647dcbbe71dfad19fc9982464bc67a25450
GeunTae You <ygeuntae1208@naver.com>
2024-10-20 15:51:41 +0900

    fix: 모듈명 수정

--- src/NLP/make_csv/okr_csv.py
+++ src/NLP/make_csv/okr_csv.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from extract.extract_kr import extract_okr
+from extract.extract_okr import extract_okr
 
 
 # 현재 SBert.py 파일 위치 기준으로 상대 경로 설정
