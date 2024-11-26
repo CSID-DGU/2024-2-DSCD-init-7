287c614e945bd104e6280fe1cfb8b37b3837c404
seeeeru <presnt0933@gmail.com>
2024-11-26 13:57:20 +0900

    왜안되지

---
+++ src/backend/model_inference.py
@@ -0,0 +1,27 @@
+import torch
+from algorithm import TeamTransformer
+from dataloader import create_test_loader
+
+
+
+def load_model(weights_path, params):
+    model = TeamTransformer(**params)
+    state_dict = torch.load(weights_path, map_location=torch.device('cpu'))
+    model.load_state_dict(state_dict)
+    model.eval()
+    return model
+
+def predict_with_model(model, data, batch_size=512):
+    test_loader = create_test_loader(data, batch_size)
+    predictions_list = []
+    transformer_out_list = []
+
+    for batch_inputs, _ in test_loader:
+        with torch.no_grad():
+            inputs = batch_inputs[:, :, :-2]
+            inputs_num = batch_inputs[:, :, -2:].int()
+            predictions, transformer_out = model(inputs)
+            predictions_list.append(predictions)
+            transformer_out_list.append(transformer_out)
+
+    return predictions_list, transformer_out_list
